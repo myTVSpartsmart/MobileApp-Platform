74fc3de0b158a86da94b06d21dc27d5f5af6d884
Sanjana Sivasankaran <sanjana.sivasankaran@tvs.in>
2025-09-10 10:01:55 +0530

    CashPayment-43-UI-Completed

--- src/routers/AuthorizedStack.jsx
+++ src/routers/AuthorizedStack.jsx
@@ -6,7 +6,7 @@ import SplashScreen from "../Screens/SplashScreen";
 import SearchCustomer from "../Screens/CreateOrderModule/SearchCustomer";
 
 import LoginScreen from "../Screens/LoginScreen";
-
+import CashPayment from "../Screens/CreateReceiptModule/CashPayment"
 import HomeScreen from "../Screens/HomeScreen";
 import TabNavigator from "../Navigation/TabNavigator";
 
@@ -46,6 +46,11 @@ const AuthorizedStack = () => {
        <Stack.Screen
         name="LoginScreen"
         component={LoginScreen}
+        options={{ headerShown: false, animation: 'slide_from_right' }}
+      />
+      <Stack.Screen
+        name="CashPayment"
+        component={CashPayment}
         options={{ headerShown: false, animation: 'slide_from_right' }}
       />
     </Stack.Navigator>
